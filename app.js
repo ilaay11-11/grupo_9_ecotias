6e5cb2e9d53db4d811b7cbb71e70bf7cad5a6105
Juan Jiménez <nefioxdev@gmail.com>
2021-06-02 00:48:25 -0500

    app.js listo

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => res.sendFile('/index.html', {root: 'views'}));
+app.get('/producto', (req, res) => res.sendFile('/productDetail.html', {root: 'views'}));
+app.get('/carrito', (req, res) => res.sendFile('/productCart.html', {root: 'views'}));
+app.get('/login', (req, res) => res.sendFile('/login.html', {root: 'views'}));
+app.get('/registro', (req, res) => res.sendFile('/register.html', {root: 'views'}));
+
+app.listen(PORT, () => console.log(`Servidor corriendo en el ${PORT}`));
