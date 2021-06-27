21040afe615295abb1676b62947f898e777b8255
Juan Jiménez <nefioxdev@gmail.com>
2021-06-26 23:11:57 -0500

    Controllers & Routes done
    Falta separar los componentes repetidos en archivos parciales

---
+++ routes/mainRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mainController = require('../controllers/mainController');
+const router = express.Router();
+
+router.get('/', mainController.home);
+router.get('/login', mainController.login);
+router.get('/carrito', mainController.productCart);
+router.get('/producto', mainController.productDetail);
+router.get('/registro', mainController.register);
+
+module.exports = router;
