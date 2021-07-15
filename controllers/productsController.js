edcd65c9dc3937b598c1d653f459f44e72ea7528
Juan Jiménez <nefioxdev@gmail.com>
2021-07-14 21:35:54 -0500

    Revisando CRUD v1

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,21 +3,13 @@ const path = require('path');
 const productsFilePath = path.join(__dirname, '../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-
 const productsController = {
-    index: (req, res) => {
-        res.render('products/products', { products })
-    },
+    index: (req, res) => res.render('products/products', { products }),
     create: (req, res) => {
         res.render('products/createProduct')
     },
     edit: (req, res) => {
-        const productId = parseInt(req.params.id);
-
-		const productToEdit = products.find((producto) => {
-			return producto.id === productId;
-		})
-        res.render('products/editProduct',{ productToEdit });
+        res.render('products/editProduct')
     },
     store: (req, res) => {
         const newInfo = req.body;
@@ -33,24 +25,19 @@ const productsController = {
 		});
 
 
-        res.render('products/productDetail', { productSelected });
+        res.render('products/productDetail', { productSelected })
     },
     update: (req, res) => {
         const productInfo = req.body;
         const productIndex = products.findIndex((producto) => {
-            return producto.id === parseInt(req.params.id);
+            return producto.id === parseInt(req.params.id)
         })
         products[productIndex] = {...products[productIndex], ...productInfo};
         fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2));
-        res.redirect('users/index');
-    },
-    destroy: (req,res) => {
-        const productIndex = products.findIndex ((producto) => {
-			return producto.id === parseInt(req.params.id);
-		});
-        products.splice(productIndex, 1);
-        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2));
         res.redirect('users/index')
+    },
+    destroy: () => {
+
     }
 }
 
