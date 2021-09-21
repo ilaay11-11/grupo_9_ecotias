90dd15c44ea693ce2b075dfcbda9358418963dfa
Juan Jiménez <nefioxdev@gmail.com>
2021-09-21 11:59:10 -0500

    DB details

--- database/config/config.js
+++ database/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "ecotias",
-    "host": "127.0.0.1",
+    "username": "sql5438965",
+    "password": "VBxtpTcjV1",
+    "database": "sql5438965",
+    "host": "sql5.freemysqlhosting.net",
     "dialect": "mysql",
     "port": "3306"
   },
@@ -15,10 +15,11 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": "sql5438965",
+    "password": "VBxtpTcjV1",
+    "database": "sql5438965",
+    "host": "sql5.freemysqlhosting.net",
+    "dialect": "mysql",
+    "port": "3306"
   }
 }
