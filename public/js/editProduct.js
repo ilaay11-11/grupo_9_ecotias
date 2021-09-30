d1e53f9f72455885b360cdd1255d7966fad14e66
aee95 <aee95.123@gmail.com>
2021-09-30 03:44:37 -0500

    Validacion de editar productos

---
+++ public/js/editProduct.js
@@ -0,0 +1,57 @@
+window.addEventListener('load', function(){
+    let nombre = document.querySelector('input.nombre');
+    let descripcion = document.querySelector('#descripcion');
+    let precio = document.querySelector('input.precio');
+    let img = document.querySelector('#newProductImage')
+    let formulario = document.querySelector('form');
+
+    formulario.onsubmit = (event)=>{
+        let errores = [];
+        let errores1 = [" ", " ", " ", " "];
+        function validateInputs(){
+            if(nombre.value ===""){
+                errores.push("Ingresa el nombre del producto")
+            } else if (nombre.value.length <5)
+                { 
+                errores.push("El nombre debe tener mínimo 5 caracteres");
+                } else {
+                    errores.push(" ");
+                }
+            if (precio.value ===""){ 
+                errores.push("Ingresa el precio del producto");
+            } else {
+                errores.push(" ");
+            }
+            if (descripcion.value.length < 20){ 
+                errores.push("La descripción debe tener al menos 20 caracteres")
+            } else {
+                errores.push(" ");
+            }
+            let extensiones = getFileExtension3(img.value.toUpperCase());
+            if(extensiones ===""){
+                errores.push(" ");
+            }else if(extensiones != "GIF" && extensiones != "PNG" && extensiones != "JPG" && extensiones != "JPEG"){
+               errores.push("El formato de la imagen no es válido");
+            } else {
+                errores.push(" ");
+            }
+        }
+        validateInputs();
+        let valid = errores.length==errores1.length && errores.every(function(v,i) { return v === errores1[i]});
+        if(errores.length > 0 && valid === false){
+            event.preventDefault();
+            let ulErrores = document.querySelector(".errores ul");
+            let ulErrores1 = document.querySelector(".errores1 ul");
+            let ulErrores2 = document.querySelector(".errores2 ul");
+            let ulErrores3 = document.querySelector(".errores3 ul");
+            ulErrores.innerHTML = errores[0]; 
+            ulErrores1.innerHTML = errores[1];   
+            ulErrores2.innerHTML = errores[2];  
+            ulErrores3.innerHTML = errores[3];        
+        }
+        
+        function getFileExtension3(filename) {
+            return filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2);
+          }
+    }
+})
