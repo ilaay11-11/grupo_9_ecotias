b743f157fd5b80cf2badbd5d346635f04b299af5
ilaay11-11 <eli11_vt@hotmail.com>
2021-09-30 09:24:22 -0500

    validación front-end createProduct v2

--- public/js/createProduct.js
+++ public/js/createProduct.js
@@ -1,31 +1,54 @@
 window.addEventListener('load', function(){
     let nombre = document.querySelector('#nombre');
     let descripcion = document.querySelector('#description');
-    let img = document.querySelector('#product_image')
+    let img = document.querySelector('#product_image');
     let formulario = document.querySelector('form');
-    let extensiones = img.substring(img.lastIndexOf('.')+1).toLowerCase();
+    
 
     formulario.onsubmit = (event)=>{
         let errores = [];
+        let errores1 = [" ", " ", " ", " "];
         function validateInputs(){
-            if(nombre.value.length <5){
-                errores.push("El nombre debe tener al menos 5 caracteres");
+            if(nombre.value === ""){
+                errores.push("Ingresa el nombre del producto");
+            } else if(nombre.value.length < 5){
+                errores.push("El nombre debe tener mínimo 5 caracteres")
+            }else{
+                errores.push(" ");
             }
-            if(descripcion.value.length <20){
+            if(descripcion.value.length < 20){
                 errores.push("La descripción debe tener al menos 20 caracteres");
+            } else {
+                errores.push(" ");
             }
-            if(extensiones != "gif" || extensiones != "png" || extensiones != "jpg" || extensiones != "jpeg"){
-                errores.push("El formato de la imagen no es válido");
+            let extensiones = getFileExtension3(img.value.toUpperCase());
+            if(extensiones ===""){
+                errores.push(" ");
+            }else if(extensiones != "GIF" && extensiones != "PNG" && extensiones != "JPG" && extensiones != "JPEG"){
+               errores.push("El formato de la imagen no es válido");
+            } else {
+                errores.push(" ");
             }
+            
         }
         validateInputs();
-        if(errores.length > 0){
+        let valid = errores.length == errores1.length && errores.every(function (v, i){
+            return v === errores1[i]
+        } );
+        if(errores.length > 0 && valid === false){
             event.preventDefault();
             let ulErrores = document.querySelector(".errores ul");
-            ulErrores.innerHTML = "";
-            errores.forEach(e => {
-                ulErrores.innerHTML += `<li>${e}`
-            });
+            let ulErrores1 = document.querySelector(".errores1 ul");
+            let ulErrores2 = document.querySelector(".errores2 ul");
+            let ulErrores3 = document.querySelector(".errores3 ul");
+            ulErrores.innerHTML = errores[0]; 
+            ulErrores1.innerHTML = errores[1];   
+            ulErrores2.innerHTML = errores[2];  
+            ulErrores3.innerHTML = errores[3];
+            
         }
+        function getFileExtension3(filename) {
+            return filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2);
+          }
     }
 })
