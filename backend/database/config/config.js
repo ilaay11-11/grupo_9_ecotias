c223f4f586745b966b63266f89accf88e8ca3be4
aee95 <aee95.123@gmail.com>
2021-10-04 21:09:16 -0500

    separando backend & frontend

--- backend/database/config/config.js
+++ backend/database/config/config.js
@@ -1,15 +1,15 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "ecotias",
-    "host": "127.0.0.1",
+    "username": "sql5438965",
+    "password": "VBxtpTcjV1",
+    "database": "sql5438965",
+    "host": "sql5.freemysqlhosting.net",
     "dialect": "mysql",
     "port": "3306"
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {Link,Route,Switch} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
