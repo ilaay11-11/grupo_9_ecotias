c272245f7198df95a2a1b043096d2925179e8eca
ilaay11-11 <eli11_vt@hotmail.com>
2021-10-10 20:50:46 -0500

    rubbish

--- backend/controllers/API/productsControllerAPI.js
+++ backend/controllers/API/productsControllerAPI.js
@@ -1,60 +1,105 @@
 const { validationResult } = require('express-validator');
+const { QueryInterface } = require('sequelize/types');
+const { sequelize, Sequelize } = require('../../database/models/');
 let db = require('../../database/models/');
 
 const productsControllerAPI = {
+    
     index: async (req, res) => {
-        await db.Producto.findAll(
-            {
-            attributes: [
-              'category_id',
-              [db.sequelize.fn('COUNT', db.sequelize.col('category_id')), 'category_id']
-            ],
-            group: ['category_id']
+        await db.Producto.findAll({
+            attributes: {
+                attributes: [
+                    'category_id',
+                    [db.sequelize.fn('COUNT', db.sequelize.col('category_id')), 'category_id']
+                ],
+                group: ['category_id'], raw: true, exclude: ['price', 'image']
             }
+        }
+
+            //attributes: [
+            //'category_id',
+            //[db.sequelize.fn('COUNT', db.sequelize.col('category_id')), 'category_id']
+            //],
+            //group: ['category_id']
+            //}
         )
-        .then(productos => {
-            let respuesta = {
-                meta: {
-                    status : 200,
-                    total: productos.length,
-                    url: 'api/productos'
-                },
-                data: productos
-            }
+            .then(productos => {
+                let respuesta = {
+                    meta: {
+                        status: 200,
+                        url: '/api/productos'
+                    },
+                    count: productos.length,
+                    //productos.forEach(info => {
+                    //return info.dataValues.count                    
+                    //}),
+                    data: productos,
+                    detail: '/api/productos/:id'
+
+                }
                 res.json(respuesta);
             });
+
+
     },
     create: async (req, res) => {
-        let categories = await db.Category.findAll();
-        let makers = await db.Maker.findAll();
-        
-        Promise.all([categories, makers])
-        .then( ([categories, makers]) => {
-            return res.render('products/createProduct', {categories, makers});
-        });
+        db.Producto
+            .create(
+                {
+                    name: req.body.name,
+                    description: req.body.description,
+                    image: req.body.image
+                }
+            )
+            .then(confirm => {
+                let respuesta;
+                if (confirm) {
+                    respuesta = {
+                        meta: {
+                            status: 200,
+                            total: confirm.length,
+                            url: 'api/productos/create'
+                        },
+                        data: confirm
+                    }
+                } else {
+                    respuesta = {
+                        meta: {
+                            status: 200,
+                            total: confirm.length,
+                            url: 'api/productos/create'
+                        },
+                        data: confirm
+                    }
+                }
+                res.json(respuesta);
+            })
+            .catch(error => res.send(error))
     },
     store: async (req, res) => {
         const resultValidation = validationResult(req);
         console.log(resultValidation);
-		if (resultValidation.errors.length > 0) {
+        if (resultValidation.errors.length > 0) {
 
             let categories = await db.Category.findAll();
             let makers = await db.Maker.findAll();
-            
+
             Promise.all([categories, makers])
-            .then( ([categories, makers]) => {
-                return res.render('products/createProduct', {
-                    errors: resultValidation.mapped(),
-                    oldData: req.body, categories, makers});
-            })
-            .catch(err => console.log(err));
+                .then(([categories, makers]) => {
+                    return res.json(
+                        {
+                            errors: resultValidation.mapped(),
+                            oldData: req.body, categories, makers
+                        });
+                })
+                .catch(err => console.log(err));
         } else {
-            if(req.file) {
+            if (req.file) {
                 await db.Producto.create({
                     name: req.body.name,
                     description: req.body.description,
                     image: req.file.filename,
-                    category_id: req.body.category,
+
                     price: req.body.price,
                     maker_id: req.body.maker
                 });
@@ -66,90 +111,111 @@ const productsControllerAPI = {
                     price: req.body.price,
                     maker_id: req.body.maker
                 });
-            
+
             };
-            res.redirect('/productos');
+            res.json(req.body);
         }
     },
     detail: async (req, res) => {
-        const productId = parseInt(req.params.id);
-        await db.Producto.findByPk(productId, {
-            include: [{association: 'category'}, {association: 'maker'}]
-        })
-        .then(function(productSelected) {
-            productSelected ? res.render('products/productDetail', { productSelected }) : res.render("products/product-not-found");
-        });
+        db.Producto.findByPk(req.params.id)
+
+            .then(producto => {
+                let respuesta = {
+                    meta: {
+                        status: 200,
+                        total: producto.length,
+                        url: '/api/movie/:id'
+                    },
+                    data: producto
+                }
+                res.json(respuesta);
+            });
     },
     edit: async (req, res) => {
-        const productId = parseInt(req.params.id);
+        let productId = parseInt(req.params.id);
 
         let product = await db.Producto.findByPk(productId, {
-            include: [{association: 'category'}, {association: 'maker'}]
+            include: [{ association: 'category' }, { association: 'maker' }]
         });
         let categories = await db.Category.findAll();
         let makers = await db.Maker.findAll();
-        
+
         Promise.all([categories, makers, product])
-        .then( ([categories, makers, product]) => {
-            return res.render('products/editProduct', {categories, makers, product});
-        });
+            .then(([categories, makers, product]) => {
+                return res.json(product);
+            });
     },
     update: async (req, res) => {
-        const resultValidation = validationResult(req);
-		if (resultValidation.errors.length > 0) {
-
-            const productId = parseInt(req.params.id);
-
-            let product = await db.Producto.findByPk(productId, {
-                include: [{association: 'category'}, {association: 'maker'}]
-            });
-            let categories = await db.Category.findAll();
-            let makers = await db.Maker.findAll();
-            
-            Promise.all([categories, makers, product])
-            .then( ([categories, makers, product]) => {
-                return res.render('products/editProduct', {
-                    errors: resultValidation.mapped(),
-                    oldData: req.body, categories, makers, product});
+        let productId = req.params.id;
+        console.log(productId)
+        console.log(req.body)
+        db.Producto.update(
+            {
+                name: req.body.name,
+                description: req.body.description,
+                image: req.body.image,
+                category_id: req.body.category,
+                price: req.body.price,
+                maker_id: req.body.maker
+            },
+            {
+                where: { id: productId }
             })
-            .catch(err => console.log(err));
-        } else {
-            if(req.file) {
-                await db.Producto.update({
-                    name: req.body.name,
-                    description: req.body.description,
-                    image: req.file.filename,
-                    category_id: req.body.category,
-                    price: req.body.price,
-                    maker_id: req.body.maker
-                }, {
-                    where: {
-                        id: req.params.id
+            .then(confirm => {
+                let respuesta;
+                if (confirm) {
+                    respuesta = {
+                        meta: {
+                            status: 200,
+                            total: confirm.length,
+                            url: 'api/productos/:id'
+                        },
+                        data: confirm
                     }
-                });
-            } else {
-                await db.Producto.update({
-                    name: req.body.name,
-                    description: req.body.description,
-                    category_id: req.body.category,
-                    price: req.body.price,
-                    maker_id: req.body.maker
-                }, {
-                    where: {
-                        id: req.params.id
+                } else {
+                    respuesta = {
+                        meta: {
+                            status: 204,
+                            total: confirm.length,
+                            url: 'api/productos/update/:id'
+                        },
+                        data: confirm
                     }
-                });
-            };
-            res.redirect('/productos/'+ req.params.id);
-        }
+                }
+                res.json(respuesta);
+            })
+            .catch(error => res.send(error))
     },
-    destroy: (req,res) => {
-        db.Producto.destroy({
-            where: {
-                id: req.params.id
-            }
-        });
-        res.redirect('/productos');
+    destroy: (req, res) => {
+        let productId = req.params.id;
+        console.log(productId)
+        db.Producto
+            .destroy({ where: { id: productId }, force: true })
+            .then(confirm => {
+                let respuesta;
+                console.log('confirm', confirm)
+                if (confirm) {
+                    respuesta = {
+                        meta: {
+                            status: 200,
+                            total: confirm.length,
+                            url: 'api/productos/delete/:id'
+                        },
+                        data: confirm
+                    }
+                } else {
+                    respuesta = {
+                        meta: {
+                            status: 204,
+                            total: confirm.length,
+                            url: 'api/productos/destroy/:id'
+                        },
+                        data: confirm
+                    }
+                }
+                res.json(respuesta);
+            })
+            .catch(error => res.send(error))
     }
 };
 
--- backend/routes/API/productsRoutesAPI.js
+++ backend/routes/API/productsRoutesAPI.js
@@ -11,19 +11,19 @@ const productsControllerAPI = require('../../controllers/API/productsControllerA
 router.get('/', productsControllerAPI.index); // MUESTRA LISTA PRODUCTOS
 
 /*** CREATE ONE PRODUCT ***/
-router.get('/crear', productsControllerAPI.create); // MUESTRA FORMULARIO DE CREACIÓN DE PRODUCTO
-router.post('/crear', uploadUserImage.single('product_image'), validationProductsMiddleware, productsControllerAPI.store); // ENVÍA INFO DE CREACIÓN DE PRODUCTO
+//router.get('/crear', productsControllerAPI.create); // MUESTRA FORMULARIO DE CREACIÓN DE PRODUCTO
+//router.post('/crear', uploadUserImage.single('product_image'), validationProductsMiddleware, productsControllerAPI.store); // ENVÍA INFO DE CREACIÓN DE PRODUCTO
 
 // /*** GET ONE PRODUCT ***/
-router.get('/:id', productsControllerAPI.detail); // MUESTRA DETALLE DE PRODUCTO
+//router.get('/:id', productsControllerAPI.detail); // MUESTRA DETALLE DE PRODUCTO
 
 // /*** EDIT ONE PRODUCT ***/
-router.get('/:id/editar', productsControllerAPI.edit); // MUESTRA FORMULARIO DE EDICIÓN DE PRODUCTO
-router.put('/:id', uploadUserImage.single('newProductImage'), validationEditProductsMiddleware, productsControllerAPI.update); // RUTA POR LA QUE VIAJAN LOS DATOS DE EDICIÓN DE PRODUCTO
+//router.get('/:id/editar', productsControllerAPI.edit); // MUESTRA FORMULARIO DE EDICIÓN DE PRODUCTO
+//router.put('/:id', uploadUserImage.single('newProductImage'), validationEditProductsMiddleware, productsControllerAPI.update); // RUTA POR LA QUE VIAJAN LOS DATOS DE EDICIÓN DE PRODUCTO
 
 
 // /*** DELETE ONE PRODUCT***/
-router.delete('/:id', productsControllerAPI.destroy); // RUTA POR LA QUE VIAJAN LOS DATOS DE PRODUCTO A ELIMINAR
+//router.delete('/:id', productsControllerAPI.destroy); // RUTA POR LA QUE VIAJAN LOS DATOS DE PRODUCTO A ELIMINAR
 
 
 module.exports = router;
